f085195fabf10bd777149bda3158b6d5402c0660
anugraha4 <96745531+anugraha4@users.noreply.github.com>
2022-12-05 14:38:43 +0530

    student

---
+++ studentnew_backend/src/main/java/com/nest/studentnew_backend/StudentnewBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.studentnew_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentnewBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentnewBackendApplication.class, args);
+	}
+
+}
---
+++ studentnew_backend/src/test/java/com/nest/studentnew_backend/StudentnewBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.studentnew_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentnewBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
